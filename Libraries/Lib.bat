a06f6f1c953b6544cc4d894c02d68d3a1a3d5179
alanliew88 <keanoon03@gmail.com>
2022-05-10 22:09:46 +0800

    allure report update

--- Libraries/Lib.bat
+++ Libraries/Lib.bat
@@ -3,4 +3,4 @@ pip install robotframework
 pip install robotframework-SeleniumLibrary
 pip install -U robotframework-pabot
 pip install allure-robotframework
-robot --listener allure_robotframework title.robot
+robot --listener allure_robotframework;./output/allure title.robot
